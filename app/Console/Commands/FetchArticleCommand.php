e12c34afbf57ea0feac5674e699a612e2f18a7b0
babak asadzadeh <babak.asad73@gmail.com>
2025-01-24 19:51:09 +0330

    add translates

--- app/Console/Commands/FetchArticleCommand.php
+++ app/Console/Commands/FetchArticleCommand.php
@@ -5,18 +5,20 @@ namespace App\Console\Commands;
 use App\Factories\NewsProviderFactory;
 use App\Repositories\Contracts\Article\ArticleRepositoryInterface;
 use Carbon\Carbon;
+use Carbon\Exceptions\InvalidFormatException;
 use Illuminate\Console\Command;
-use Mockery\Exception;
 
 class FetchArticleCommand extends Command
 {
+    private $limit = 100;
+
     /**
      * The name and signature of the console command.
      * Providers should separate with comma example: "newsapi,nyt".
      *
      * @var string
      */
-    protected $signature = 'app:fetch-article {providers}';
+    protected $signature = 'app:fetch-article {providers : list of providers seperated by comma} {from? : start of period} {to? : end of period}';
 
     /**
      * The console command description.
@@ -37,16 +39,31 @@ class FetchArticleCommand extends Command
      */
     public function handle()
     {
-        $providers = explode(',', $this->argument('providers'));
+        try {
+
+            $providers = explode(',', $this->argument('providers'));
+            $from = Carbon::parse($this->argument('from')) ?? Carbon::now()->subHour()->startOfHour();
+            $to = Carbon::parse($this->argument('to')) ?? Carbon::now()->startOfHour();
+
+            if ($from->gte($to)) {
+                $this->info(__('messages.errors.invalid_date'));
+                return Command::FAILURE;
+            }
+
+        } catch (InvalidFormatException $exception) {
+            report($exception);
+            $this->error(__('messages.errors.invalid_date'));
+            return Command::FAILURE;
+        }
+
         foreach ($providers as $provider) {
             try {
                 $fetcher = NewsProviderFactory::create($provider);
 
-                $result = $fetcher->fetchNews(100, Carbon::now()->subHour()->startOfHour()->format('Y-m-d'),
-                    Carbon::now()->startOfHour()->format('Y-m-d'));
+                $result = $fetcher->fetchNews($this->limit, $from->format('Y-m-d'), $to->format('Y-m-d'));
 
                 $this->articleRepository->insert($result);
-            } catch (Exception $exception) {
+            } catch (\Throwable $exception) {
                 report($exception);
             }
         }
---
+++ resources/lang/en/messages.php
@@ -0,0 +1,19 @@
+<?php
+return $messages = [
+    'errors' => [
+        'invalid_date' => 'invalid date was given',
+    ],
+    'response' => [
+        'success' => 'operation successfully done',
+        'failed' => 'operation failed',
+        'OK' => 'ok',
+        'NotFound' => 'not found',
+        'BadRequest' => 'bad request',
+        'Unauthorized' => 'unauthorized',
+        'Forbidden' => 'forbidden',
+        'NotAllowed' => 'not allowed',
+        'UnProcessableEntity' => 'un processable entity',
+        'failedStatus' => 'failed',
+        'successStatus' => 'success',
+    ],
+];
